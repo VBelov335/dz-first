24429cdda4a4048b1a3a7f22fcb0c4e7c15333eb
Vladislav <belovv296@gmail.com>
2024-04-11 00:08:02 +0600

    4-navigator

---
+++ app.js
@@ -0,0 +1,11 @@
+// Старт
+const positionLat = 4;
+const positionLong = 0;
+
+// Назначение
+const addressLat = 0;
+const addressLong = 3;
+
+
+console.log((positionLong - positionLat)**2 + (addressLat - addressLong)**2)
+console.log(Math.sqrt(25))
